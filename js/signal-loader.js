f73a845a56ec083b13a011ea0ebd9e4fd3263817
TheCryptomath <100493215+TheCryptomath@users.noreply.github.com>
2026-06-16 14:55:58 +0200

    Add files via upload

--- js/signal-loader.js
+++ js/signal-loader.js
@@ -31,7 +31,7 @@
     wrap.classList.add('is-loading-world');
 
     loadScript('/js/three.min.js?v=0.128.0')
-      .then(() => loadScript('/js/signal-world.js?v=20260616-3d-lazy'))
+      .then(() => loadScript('/js/signal-world.js?v=20260616-portal-final'))
       .then(() => wrap.classList.remove('is-loading-world'))
       .catch(() => {
         wrap.classList.remove('is-loading-world');
